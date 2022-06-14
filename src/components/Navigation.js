0dcf80e83314e25aa4597bcabd6aa3f70115e5b4
Sam Dunegan <cesarcisneros9@gmail.com>
2022-06-14 10:36:59 -0500

    updated navigation component to use useNavigate instead of window.reload, used cookie package to delete cookie

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,43 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton, 
-  Typography
-} from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import cookie from "cookie";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navigation = () => {
-    return (
-        <AppBar position="relative">
-            <Toolbar>
-                <IconButton color="inherit">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" style={{ flexGrow: "1" }}>
-                    FakeCars.com
-                </Typography>
-                <ul className="nav-list">
-                    <li className="nav-list-item">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="nav-list-item">
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li className="nav-list-item"
-                        onClick={() => {
-                            document.cookie = "loggedIn="
-                            window.location.replace("/login")
-                        }}>
-                        Logout
-                    </li>
-                </ul>
-            </Toolbar>
-        </AppBar>
-    )
-}
+  const navigate = useNavigate();
 
-export default Navigation
+  return (
+    <AppBar position="relative">
+      <Toolbar>
+        <IconButton color="inherit">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" style={{ flexGrow: "1" }}>
+          FakeCars.com
+        </Typography>
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-list-item">
+            <Link to="/about">About</Link>
+          </li>
+          <li
+            className="nav-list-item"
+            onClick={() => {
+              document.cookie = cookie.serialize("loggedIn", null, {
+                maxAge: 0,
+              });
+              navigate("/login");
+            }}
+          >
+            Logout
+          </li>
+        </ul>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navigation;
